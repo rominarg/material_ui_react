bb9a7b82eb94e32af1b4cfdc16f80007f2c06499
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-03 13:41:44 -0300

    Card mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import { Container } from "@mui/material";
 import Product from "./Components/product";
+import MarketCard from "./Components/MarketCard";
 
 export default function App() {
   return(
     <Container maxWidth="xl">
       <Product />
+      <MarketCard />
     </Container>
     
   )
