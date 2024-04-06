2fde5047977271e945bb5d4ba732120b984a06ea
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-05 22:01:44 -0300

    banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,11 +2,26 @@ import navi_honda from "../assets/img/navi_honda.jpg";
 
 function Banner() {
   return (
-    <div>
-<img src={navi_honda} alt="" />
+    <div style={styles.bannerContainer}>
+      <img src={navi_honda} alt="Navi Honda" style={styles.image} />
     </div>
-     
   );
 }
+
+const styles = {
+  bannerContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "500px", 
+    backgroundColor: "#f0f0f0", 
+    marginBottom: "20px", 
+    marginTop: "60px", 
+  },
+  image: {
+    maxWidth: "100%", 
+    maxHeight: "100%",
+  },
+};
 
 export default Banner;
