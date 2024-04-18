b75c1a57da6e832933dd349fdd490360208ab329
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-18 01:10:27 -0300

    accordion

--- src/Components/FooterMob.jsx
+++ src/Components/FooterMob.jsx
@@ -4,7 +4,9 @@ import AccordionActions from '@mui/material/AccordionActions';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Button from '@mui/material/Button';
+
+import { Divider, Grid, Link, Typography } from '@mui/material';
+import DATAWEB from "../assets/img/DATAWEB.jpg";
 
 export default function FooterMob() {
   return (
@@ -15,11 +17,27 @@ export default function FooterMob() {
           aria-controls="panel1-content"
           id="panel1-header"
         >
-          Accordion 1
+          Products
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Honda</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Honda Autos</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Honda Motos</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Honda Productos de Fuerza</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Honda Marine</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Honda Racing</Link>
+              </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -28,30 +46,40 @@ export default function FooterMob() {
           aria-controls="panel2-content"
           id="panel2-header"
         >
-          Accordion 2
+          Useful links
         </AccordionSummary>
         <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Off Road</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">ATV</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Concesionarios</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Novedades</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Post Venta</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Contacto</Link>
+              </Typography>
+              <Typography variant="body2" paragraph>
+                <Link href="#" color="inherit">Recursos Humanos</Link>
+              </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3-content"
-          id="panel3-header"
-        >
-          Accordion Actions
-        </AccordionSummary>
-        <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-        <AccordionActions>
-          <Button>Cancel</Button>
-          <Button>Agree</Button>
-        </AccordionActions>
-      </Accordion>
+      <Grid item xs={12} sm={6} md={3}>
+              <Typography variant="h6" gutterBottom>Contact</Typography>
+              <Divider style={{ backgroundColor: '#fff', width: '60px', height: '2px', marginBottom: '1rem' }} />
+              <Typography variant="body2" paragraph><i className="fas fa-home mr-3"></i> New York, NY 10012, US</Typography>
+              <Typography variant="body2" paragraph><i className="fas fa-envelope mr-3"></i> info@example.com</Typography>
+              <Typography variant="body2" paragraph><i className="fas fa-phone mr-3"></i> 08001333897</Typography>
+              <img src={DATAWEB} alt="Foto de contacto" style={{ width: '90px', height: 'auto', marginTop: '1rem' }} />
+            </Grid>
     </div>
   );
 }
